00988962487d9654ad4d7c950d2c59152c2ca62e
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-01-24 10:12:10 -0200

    Adicionei uma função chamada Cipher e uma função para o botão enviar

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,23 @@
-window.cipher = {
+/* window.cipher = {
   
 };
+*/
 
+function cipher(phrase, desloc){
+    console.log(phrase, desloc);
+    let finalPhrase = "";
+    for (let i = 0; i < phrase.length; i++){
+        let char = phrase.charCodeAt(i);
+        let deslocChar = parseInt(char) + parseInt(desloc);
+        let cipherChar = String.fromCharCode(deslocChar);
+        finalPhrase = finalPhrase + cipherChar;
+    }
+    return finalPhrase;
+}
+
+function encode(){
+    let phraseNormal = document.getElementById("phrase").value;
+    let deslocamento = document.getElementById("desloc").value;
+    let resultado = cipher(phraseNormal, deslocamento);
+    document.getElementById("phraseEncoded").innerHTML = resultado;
+}
