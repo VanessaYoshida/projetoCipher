b92c238778699a801d533e1dbac66a95ab5c5076
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-01-25 16:08:09 -0200

    adicionado na função os ifs para funcionar com letras maiusculas e minusculas, e estou tentando fazer uma maneira de de quando for Mod 26=0 ele dar outro resultado

--- src/cipher.js
+++ src/cipher.js
@@ -4,13 +4,26 @@
 */
 
 function cipher(phrase, desloc){
-    console.log(phrase, desloc);
     let finalPhrase = "";
+
     for (let i = 0; i < phrase.length; i++){
+        let cipherChar = "";
+
         let char = phrase.charCodeAt(i);
-        let deslocChar = parseInt(char) + parseInt(desloc);
-        let cipherChar = String.fromCharCode(deslocChar);
-        finalPhrase = finalPhrase + cipherChar;
+        if (char > 64 && char < 91 && char != (char % 26 == 0) ){
+            let deslocChar = (((parseInt(char)- 65) + parseInt(desloc)) % 26) + 65;
+            if (char = (char % 26 == 0)){
+                deslocChar = deslocChar + 3;
+            }
+            cipherChar = String.fromCharCode(deslocChar);
+            finalPhrase = finalPhrase + cipherChar;
+        }
+        if (char > 96 && char < 123 && char != (char % 26 == 0){
+            let deslocChar = (((parseInt(char)- 97) + parseInt(desloc)) % 26) + 97;
+            cipherChar = String.fromCharCode(deslocChar);
+            finalPhrase = finalPhrase + cipherChar;
+        }
+      
     }
     return finalPhrase;
 }
@@ -21,3 +34,9 @@ function encode(){
     let resultado = cipher(phraseNormal, deslocamento);
     document.getElementById("phraseEncoded").innerHTML = resultado;
 }
+
+
+
+ 
+
+    
