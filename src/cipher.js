0037713c67128db8bdd24f91ca33846fe4c556e5
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-01-26 17:01:39 -0200

    Adicionado função para botão descodificar

--- src/cipher.js
+++ src/cipher.js
@@ -3,10 +3,10 @@
 };
 */
 function encode(){
-    let phraseNormal = document.getElementById("phrase").value;
-    let deslocamento = document.getElementById("desloc").value;
-    let resultado = cipher(phraseNormal, deslocamento);
-    document.getElementById("phraseEncoded").innerHTML = resultado;
+    let phraseTyped = document.getElementById("phrase").value;
+    let displacement = document.getElementById("desloc").value;
+    let result = cipher(phraseTyped, displacement);
+    document.getElementById("phraseEncoded").innerHTML = result;
 }
 
 function cipher(phrase, desloc){
@@ -45,7 +45,49 @@ function cipher(phrase, desloc){
     return finalPhrase;
 }
 
+function decode(){
+    let phraseCoded = document.getElementById("codedPhrase").value;
+    let code = document.getElementById("deslocCode").value;
+    let resultado = decodePhrase(phraseCoded, code);
+    document.getElementById("phraseDecoded").innerHTML = resultado;
+}
 
+function decodePhrase(phrase, desloc){
+    let finalPhrase = "";
+    
+    for (let i = 0; i < phrase.length; i++){
+        let deslocChar = "";
+        let cipherChar = "";
+
+        let char = phrase.charCodeAt(i);
+        char = parseInt(char);
+        desloc = parseInt(desloc);
+        //let especial = phrase;
+                
+        if (char > 64 && char < 91) { 
+            if (desloc % 26 == 0){
+                deslocChar = (((char - 90 - 2) - desloc) % 26) + 90;
+        }
+            else {
+                deslocChar = (((char - 90) - desloc) % 26) + 90;
+            }
+
+            cipherChar = String.fromCharCode(deslocChar);
+            finalPhrase = finalPhrase + cipherChar;
+        }
+        if (char > 96 && char < 123) {
+            if (desloc % 26 == 0){
+                deslocChar = (((char - 122 - 2) - desloc) % 26) + 122;
+            }
+            else {
+                deslocChar = (((char - 122) - desloc) % 26) + 122;
+            }
+            cipherChar = String.fromCharCode(deslocChar);
+            finalPhrase = finalPhrase + cipherChar;
+        }
+    }
+    return finalPhrase;
+}
 
 /*
         else if (!string || !/^[a-zA-Z\s]*$/.test(string)) {
