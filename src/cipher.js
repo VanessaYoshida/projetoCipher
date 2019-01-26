66636f02ca2409e120e673ee22ad556199bd8480
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-01-25 22:41:32 -0200

    Modificando os if's

--- src/cipher.js
+++ src/cipher.js
@@ -7,21 +7,30 @@ function cipher(phrase, desloc){
     let finalPhrase = "";
 
     for (let i = 0; i < phrase.length; i++){
+        let deslocChar = "";
         let cipherChar = "";
 
         let char = phrase.charCodeAt(i);
-        if (char > 64 && char < 91 && char != (char % 26 == 0) ){
-            let deslocChar = (((parseInt(char)- 65) + parseInt(desloc)) % 26) + 65;
+        if (char > 64 && char < 91 && char != (char % 26 == 0)) { 
+            deslocChar = (((parseInt(char)- 65) + parseInt(desloc)) % 26) + 65;
             if (char = (char % 26 == 0)){
                 deslocChar = deslocChar + 3;
             }
             cipherChar = String.fromCharCode(deslocChar);
             finalPhrase = finalPhrase + cipherChar;
         }
-        if (char > 96 && char < 123 && char != (char % 26 == 0){
-            let deslocChar = (((parseInt(char)- 97) + parseInt(desloc)) % 26) + 97;
+        if (char > 96 && char < 123 && char != (char % 26 == 0)) {
+            deslocChar = (((parseInt(char)- 97) + parseInt(desloc)) % 26) + 97;
+            if (char = (char % 26 == 0)){
+                deslocChar = deslocChar + 3;
+            }
             cipherChar = String.fromCharCode(deslocChar);
             finalPhrase = finalPhrase + cipherChar;
+        }
+        else {
+        deslocChar = parseInt(char) + parseInt(desloc);
+        cipherChar = String.fromCharCode(deslocChar);
+        finalPhrase = finalPhrase + cipherChar;
         }
       
     }
