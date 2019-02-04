130533fd3d995c97ce48098a67be6bf01a1ff470
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-02-03 23:34:49 -0200

    alterando variavel para let dos testes

--- src/tests.js
+++ src/tests.js
@@ -1,89 +1,89 @@
 
 // Cifrar frase
-var resultado = cipher.encode("aBc", 1);
-if (resultado === "bCd") {
+let resultTest1 = cipher.encode("aBc", 1);
+if (resultTest1 === "bCd") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para aBc deslocamento 1 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para aBc deslocamento 1 retornou ${resultTest1}');
 }
 
-var resultado = cipher.encode("z", 27);
-if (resultado === "a") {
+let resultTest2 = cipher.encode("z", 27);
+if (resultTest2 === "a") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para z deslocamento 27 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para z deslocamento 27 retornou ${resultTest2}');
 }
 
-var resultado = cipher.encode("A B", 53);
-if (resultado === "B C") {
+let resultTest3 = cipher.encode("A B", 53);
+if (resultTest3 === "B C") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para A B deslocamento 53 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para A B deslocamento 53 retornou ${resultTest3}');
 }
 
 // Cifrar frase negativo
-var resultado = cipher.encode("M", -10);
-if (resultado === "C") {
+let resultTest4 = cipher.encode("M", -10);
+if (resultTest4 === "C") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para M deslocamento -10 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para M deslocamento -10 retornou ${resultTest4}');
 }
 
-var resultado = cipher.encode("M", -53);
-if (resultado === "L") {
+let resultTest5 = cipher.encode("M", -53);
+if (resultTest5 === "L") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para M deslocamento -53 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para M deslocamento -53 retornou ${resultTest5}');
 }
 
 
-var resultado = cipher.encode("M", -2599);
-if (resultado === "N") {
+let resultTest6 = cipher.encode("M", -2599);
+if (resultTest6 === "N") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para M deslocamento -2599 retornou" ${resultado}');
+    console.log('A função cypher retornou um erro: Para M deslocamento -2599 retornou" ${resultTest6}');
 }
  
 // Decifrar frase
-var resultado = cipher.decode("bCd", 1);
-if (resultado === "aBc") {
+let resultTest7 = cipher.decode("bCd", 1);
+if (resultTest7 === "aBc") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para bCd deslocamento 1 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para bCd deslocamento 1 retornou ${resultTest7}');
 }
 
-var resultado = cipher.decode("a", 53);
-if (resultado === "z") {
+let resultTest8 = cipher.decode("a", 53);
+if (resultTest8 === "z") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para a deslocamento 53 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para a deslocamento 53 retornou ${resultTest8}');
 }
 
-var resultado = cipher.decode("B C", 1);
-if (resultado === "A B") {
+let resultTest9 = cipher.decode("B C", 1);
+if (resultTest9 === "A B") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para B C deslocamento 1 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para B C deslocamento 1 retornou ${resultTest9}');
 }
 
 // Decifrar frase negativo
-var resultado = cipher.decode("zAb", -1);
-if (resultado === "aBc") {
+let resultTest10 = cipher.decode("zAb", -1);
+if (resultTest10 === "aBc") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para zAb deslocamento -1 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para zAb deslocamento -1 retornou ${resultTest10}');
 }
 
-var resultado = cipher.decode("cd E", -53);
-if (resultado === "de F") {
+let resultTest11 = cipher.decode("cd E", -53);
+if (resultTest11 === "de F") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para cd E deslocamento -53 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para cd E deslocamento -53 retornou ${resultTest11}');
 }
 
-var resultado = cipher.decode("cde", -79);
-if (resultado === "def") {
+let resultTest12 = cipher.decode("cde", -79);
+if (resultTest12 === "def") {
     console.log("Teste passou");
 } else {
-    console.log('A função cypher retornou um erro: Para cde deslocamento -79 retornou ${resultado}');
+    console.log('A função cypher retornou um erro: Para cde deslocamento -79 retornou ${resultTest12}');
 }
