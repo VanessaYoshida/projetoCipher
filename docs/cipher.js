787b0f0becf3518956927294e4fc413aab4af6c8
Vanessa <vanessayoshida92@gmail.com>
2019-06-23 17:05:00 -0300

    Instalado Eslint para formataçao de arquivo js

--- docs/cipher.js
+++ docs/cipher.js
@@ -1,49 +1,43 @@
 let cipher = {};
-
 let conversion = (category) => {
-    let phraseTyped = document.getElementById("phrase").value;
-    let displacement = document.getElementById("desloc").value;
-    let result = cipher.conversion(phraseTyped, displacement, category);
-    document.getElementById("phraseEncoded").innerHTML = result;
-}
-
+  let phraseTyped = document.getElementById('phrase').value;
+  let displacement = document.getElementById('desloc').value;
+  let result = cipher.conversion(phraseTyped, displacement, category);
+  document.getElementById('phraseEncoded').innerHTML = result;
+};
 cipher.conversion = (string, offset, category) => {
-    let finalPhrase = [];
-    let deslocChar = "";
-    let cipherChar = "";
+  let finalPhrase = [];
+  let deslocChar = '';
+  let cipherChar = '';
+  offset = parseInt(offset);
+  for (let letter in string) {
+    let char = string.charCodeAt(letter);
+    char = parseInt(char);
     offset = parseInt(offset);
-    
-    for (let i in string){
-        let char = string.charCodeAt(i);
-        char = parseInt(char);
-        offset = parseInt(offset);
-        while (offset < 0){
-            offset += 26;   
-        }    
-        if(char > 64 && char < 91){ 
-            deslocChar = category === 'codificar' ?
-            (((char - 65) + offset) % 26) + 65 :
-            (((char - 90) - offset) % 26) + 90;
-            cipherChar = String.fromCharCode(deslocChar);
-            finalPhrase.push(cipherChar);
-        }
-        else if (char > 96 && char < 123) {
-            deslocChar = category === 'codificar' ?
-            (((char - 97) + offset) % 26) + 97 :
-            (((char - 122) - offset) % 26) + 122;
-            cipherChar = String.fromCharCode(deslocChar);
-            finalPhrase.push(cipherChar);
-        }
-        else {
-            cipherChar = String.fromCharCode(char);
-            finalPhrase.push(cipherChar);
-        }
+    while (offset < 0) {
+      offset += 26;   
+    }    
+    if (char > 64 && char < 91) { 
+      deslocChar = category === 'codificar' ?
+        (((char - 65) + offset) % 26) + 65 :
+        (((char - 90) - offset) % 26) + 90;
+      cipherChar = String.fromCharCode(deslocChar);
+      finalPhrase.push(cipherChar);
+    } else if (char > 96 && char < 123) {
+      deslocChar = category === 'codificar' ?
+        (((char - 97) + offset) % 26) + 97 :
+        (((char - 122) - offset) % 26) + 122;
+      cipherChar = String.fromCharCode(deslocChar);
+      finalPhrase.push(cipherChar);
+    } else {
+      cipherChar = String.fromCharCode(char);
+      finalPhrase.push(cipherChar);
     }
-    return finalPhrase.join('');
-}
-
-clean = () => {
-    document.querySelector("#phrase").value = "";
-    document.querySelector("#desloc").value = "";
-    document.getElementById("phraseEncoded").innerHTML = "";
-}    
+  }
+  return finalPhrase.join('');
+};
+let clean = () => {
+  document.querySelector('#phrase').value = '';
+  document.querySelector('#desloc').value = '';
+  document.getElementById('phraseEncoded').innerHTML = '';
+};    
